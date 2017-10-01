7d4dbb085dfb432051d97780da29d79c35dd76ec
George Higbie <gehigbie@gmail.com>
2017-09-30 20:59:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/georgehigbie/swoosh/WecomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.georgehigbie.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WecomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wecome)
+    }
+}
