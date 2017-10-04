11eee6e0996264867c09861b3066ed1c90f0ec21
George Higbie <gehigbie@gmail.com>
2017-10-03 20:49:42 -0600

    Add new activity and layout

--- app/src/main/java/com/example/georgehigbie/swoosh/BaseActivity.kt
+++ app/src/main/java/com/example/georgehigbie/swoosh/BaseActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class BaseActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
     val TAG = "LifeCycle"
 
