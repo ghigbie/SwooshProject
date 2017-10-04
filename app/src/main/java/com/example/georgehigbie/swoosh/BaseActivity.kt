f25afc26ba7de99baaadacd7964d6ff4df7d2189
George Higbie <gehigbie@gmail.com>
2017-10-03 20:38:19 -0600

    Add logs

--- app/src/main/java/com/example/georgehigbie/swoosh/BaseActivity.kt
+++ app/src/main/java/com/example/georgehigbie/swoosh/BaseActivity.kt
@@ -2,10 +2,39 @@ package com.example.georgehigbie.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class BaseActivity : AppCompatActivity() {
 
+    val TAG = "LifeCycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "${javaClass.simpleName} OnCreate")
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart(){
+        Log.d(TAG, "${javaClass.simpleName} OnStart")
+        super.onStart()
+    }
+
+    override fun onPause(){
+        Log.d(TAG, "${javaClass.simpleName} OnPause")
+        super.onPause()
+    }
+
+    override fun onResume(){
+        Log.d(TAG, "${javaClass.simpleName} OnResume")
+        super.onResume()
+    }
+
+    override fun onStop(){
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy(){
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        super.onDestroy()
     }
 }
