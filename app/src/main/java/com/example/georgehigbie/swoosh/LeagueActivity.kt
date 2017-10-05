b2a80a5f6d989b66289c1ea56893c5a74c95a971
George Higbie <gehigbie@gmail.com>
2017-10-04 23:14:35 -0600

    Add onclick listener to oncreate method in league activity

--- app/src/main/java/com/example/georgehigbie/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/georgehigbie/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.example.georgehigbie.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        nextButton.setOnClickListener {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            startActivity(skillIntent)
+        }
     }
 }
