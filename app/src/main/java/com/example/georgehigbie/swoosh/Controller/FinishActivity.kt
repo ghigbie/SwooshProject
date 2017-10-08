ea431980e6c078748447d317cafc31b40a8040c1
George Higbie <gehigbie@gmail.com>
2017-10-07 19:59:43 -0600

    Add Finish activity

---
+++ app/src/main/java/com/example/georgehigbie/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,13 @@
+package com.example.georgehigbie.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.georgehigbie.swoosh.R
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+    }
+}
