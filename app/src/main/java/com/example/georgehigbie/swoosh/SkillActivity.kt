b0cf2198b64e378f43d0dfffc85b2316578bef6b
George Higbie <gehigbie@gmail.com>
2017-10-07 19:13:05 -0600

    Add toast for skill level

--- app/src/main/java/com/example/georgehigbie/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/georgehigbie/swoosh/SkillActivity.kt
@@ -2,11 +2,32 @@ package com.example.georgehigbie.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        var skillLevel = ""
+
+        fun skillLevelToast(skillLevel: String){
+            var toastMessage = "You have the selected the ${skillLevel} skill level."
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        }
+
+        beginnerButton.setOnClickListener {
+            ballerButton.isChecked = false
+            skillLevel = "beginner"
+            skillLevelToast(skillLevel)
+        }
+
+        ballerButton.setOnClickListener {
+            beginnerButton.isChecked = false
+            skillLevel = "baller"
+            skillLevelToast(skillLevel)
+        }
     }
 }
